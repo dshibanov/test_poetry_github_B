beb779fabbbe1c789643f48d3600ba7f4164edc4
Dmitrii 108 <dmitry.shibanov@gmail.com>
2023-06-29 15:24:27 +0530

    add TLine

---
+++ test_poetry_github_b/tline.py
@@ -0,0 +1,17 @@
+
+class TLine:
+
+    def __init__(self, params):
+        print(params)
+        print('Hail line')
+
+    def search(self, seria, params):
+        # do line search here
+        print('search')
+
+    def get_top_line(self, seria, params):
+        return top_line
+
+
+if __name__ == "__main__":
+    line = TLine({})
